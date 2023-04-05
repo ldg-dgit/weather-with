c6e05bdd8734cb20832cc92a4c88922135b8e748
Bobby Lee <ldg@dgit.co>
2023-04-05 16:58:53 +0900

    init styles

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 4, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 0.5, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <StatusBar style='light' />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "teal",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 150,
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 50,
+  },
+});
